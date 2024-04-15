29688d39e2be03f26d6498be9f4b4382b4622172
melmelchew <melchew66@gmail.com>
2024-04-15 18:21:03 +0800

    updated ui code

---
+++ server/ourDetox.py
@@ -0,0 +1,19 @@
+from transformers import BartForConditionalGeneration, BartTokenizer
+import torch
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+tokenizer = BartTokenizer.from_pretrained("facebook/bart-base")
+model = BartForConditionalGeneration.from_pretrained("facebook/bart-base")
+model.load_state_dict(torch.load("bartDetox.pt", map_location=device), strict=False)
+model.to(device)
+model.eval()
+
+def ourdetox(text):
+    toxics = [text]
+    tokens = tokenizer(toxics, return_tensors='pt', padding=True)
+    tokens = model.generate(**tokens, num_return_sequences=5, do_sample=False,
+                            temperature=1.0, repetition_penalty=10.0,
+                            max_length=128, num_beams=10)
+    neutrals = tokenizer.decode(tokens[0, ...], skip_special_tokens=True)
+    return neutrals
+
+ourdetox("fuck you motherfucker")
--- server/translate.py
+++ server/translate.py
@@ -1,17 +1,20 @@
 import sys
 from toxicScore import predict_toxicity
 from detox import detox
-
+from ourDetox import ourdetox
+import re 
 def translate(input_text):
     # Your translation logic here
-    translated_text = detox(input_text)
+    # cleaned_input_text = re.sub('\W+','', input_text)
+    # translated_text = detox(input_text)
+    translated_text = ourdetox(input_text)
     # Your toxicity scoring logic here
     class_item, toxicity_score = predict_toxicity(input_text)
 
     return translated_text, toxicity_score
 
 if __name__ == "__main__":
-    # input_text = input("Give text:")
-    input_text = sys.argv[1]
+    input_text = input("Give text:")
+    # input_text = sys.argv[1]
     translated_text, toxicity_score = translate(input_text)
     print(f"{translated_text},{toxicity_score}")
